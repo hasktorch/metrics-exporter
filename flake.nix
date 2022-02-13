ef78a8900c4ed42f26ce5e5bfada5c81dbeb0f03
Junji Hashimoto <junji.hashimoto@gmail.com>
2022-02-13 22:52:18 +0900

    Add wandb in flake.nix

--- flake.nix
+++ flake.nix
@@ -13,18 +13,38 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
+          docker-pycreds = with pkgs.python3Packages;
+            buildPythonPackage rec {
+              pname = "docker-pycreds";
+              version = "0.4.0";
+              src = builtins.fetchurl {
+                name = "docker_pycreds-0.4.0-py2.py3-none-any.whl";
+                url = "https://files.pythonhosted.org/packages/f5/e8/f6bd1eee09314e7e6dee49cbe2c5e22314ccdb38db16c9fc72d2fa80d054/docker_pycreds-0.4.0-py2.py3-none-any.whl";
+                sha256 = "0jdg6wynr0lvw4l2m4fmnz9049vj1p6ikv06a406hy32d0j12rkj";
+              };
+              format = "wheel";
+              doCheck = false;
+              propagatedBuildInputs = [six];
+            };
+          wandb = with pkgs.python3Packages;
+            buildPythonPackage rec {
+              pname = "wandb";
+              version = "0.12.10";
+              src = builtins.fetchurl {
+                name = "wandb-0.12.10-py2.py3-none-any.whl";
+                url = "https://files.pythonhosted.org/packages/05/39/d73e9a767184e0e1798255aed78e05ed4ebe988c0552f9abd36127dc2c53/wandb-0.12.10-py2.py3-none-any.whl";
+                sha256 = "07frmiq712nsphs3wj8n1hdssd9z64dp6m23m9ydpzl7fcn2y6px";
+              };
+              format = "wheel";
+              doCheck = false;
+              propagatedBuildInputs = [click shortuuid sentry-sdk requests docker-pycreds dateutil GitPython promise pyyaml psutil protobuf yaspin pathtools];
+            };
           myPython = pkgs.python39.withPackages (ps: with ps;
-            [ opencv4
-              pillow
-              pytorch-bin
+            [ pytorch-bin
               tensorflow-tensorboard
               numpy
               pandas
-              fire
-              keras
-              ijson
-              scikitimage
-              tqdm
+              wandb
             ]
           );
       in
